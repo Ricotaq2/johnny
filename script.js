19badcca4b1e848185227a2a5342c86e2e9621f6
Ricotaq2 <etaquet@yahoo.fr>
2025-12-17 14:33:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const questions = [
 
  {phrase: ".. De semaines inutiles en futiles dimanches ..",options: ["J'en parlerai au diable", "Rien à personne", "J'oublierai ton nom"],correctAnswer: "J'oublierai ton nom"},
  {phrase: ".. Pour faire bouger mes doigts pour faire vibrer mon corps ..",options: ["Je T'attends", "J'ai oublié de vivre", "Tous ensemble"],correctAnswer: "J'ai oublié de vivre"},
- {phrase: ".. A force de mots sans qu’on les écoute ..",options: ["Un cri", "L'envie", "Je T'attends"],correctAnswer: "Je T'attends"},
+ {phrase: ".. A force de mots sans qu'on les écoute ..",options: ["Un cri", "L'envie", "Je T'attends"],correctAnswer: "Je T'attends"},
  {phrase: ".. Car dans tes mains je meurs chaque soir ..",options: ["Gabrielle", "Bonne chance", "Tous ensemble"],correctAnswer: "Gabrielle"},
  {phrase: ".. Les pleurs qu'on garde sur le cœur et des regrets et des rancœurs",options: ["Chanter pour toi", "Derrière l'amour", "Deux étrangers"],correctAnswer: "Derrière l'amour"},
  {phrase: ".. Moulée dans un t-shirt et un vieux jean usé..",options: ["Comme un fou", "Un cri", "Comme un roc"],correctAnswer: "Comme un fou"},
@@ -12,7 +12,7 @@ const questions = [
  {phrase: ".. Un espoir décimé sur les pages blanches d'un monde aussi pressé..",options: ["Je ne suis qu'un homme", "Comme un fou", "Je pars demain"],correctAnswer: "Je ne suis qu'un homme"},
  {phrase: ".. Il y a longtemps sur des guitares des mains noires ..",options: ["Le rock'n'roll est né", "La musique que j'aime", "Le coeur du Rock'n'roll"],correctAnswer: "La musique que j'aime"},
  {phrase: ".. Où les hommes comme des chiens se tuent pour des mots, pour du pain ..",options: ["Le ghetto", "La prison", "Le penitencier"],correctAnswer: "Le ghetto"},
- {phrase: ".. Sans abri ni foi ni loi ce qui m’a fait vivre était en moi ..",options: ["Laisse-moi tomber", "Laura", "Les filles du paradis"],correctAnswer: "Laura"},
+ {phrase: ".. Sans abri ni foi ni loi ce qui m'a fait vivre était en moi ..",options: ["Laisse-moi tomber", "Laura", "Les filles du paradis"],correctAnswer: "Laura"},
  {phrase: ".. Dans un bruit de tonnerre à deux pas du soleil ..",options: ["Le mirage", "Les chevaliers du ciel", "Plus haut"],correctAnswer: "Les chevaliers du ciel"},
  {phrase: ".. Et la fille qui l'attend, qui l'aime comme un enfant ..",options: ["Twist à Saint-Tropez", "Les larmes de gloire", "Le chanteur abandonné"],correctAnswer: "Le chanteur abandonné"},
  {phrase: ".. Tu triches au jeu et dans le pays on te surnomme partout Le maudit ..",options: ["Les bras en croix", "Laisse-moi une chance", "Le temps passe"],correctAnswer: "Les bras en croix"},
@@ -20,7 +20,11 @@ const questions = [
  {phrase: ".. Qu'ailleurs ils aillent se faire pendre ..",options: ["Pour Exister", "Le pénitencier", "Requiem pour un fou"],correctAnswer: "Requiem pour un fou"},
  {phrase: ".. Et que tu deviens chienne ..",options: ["Que Je T'aime", "Quand Un Homme Devient Fou", "Sally"],correctAnswer: "Que Je T'aime"},
  {phrase: ".. J'ai pas toujours trouvé les mots pour bercer tes rêves d'enfants ..",options: ["Sans une larme", "S'il n'est pas trop tard", "Sang pour sang"],correctAnswer: "Sang pour sang"},
-
+ {phrase: ".. Pour que j'aime le silence qu'on me fasse des discours..",options: ["Je serai là", "Je ne suis qu'un homme", "l'envie"],correctAnswer: "l'envie"},
+ {phrase: ".. J'y crois comme un enfant, comme on peut croire au ciel..",options: ["Je te promets", "Laisse-moi une chance", "Je suis seul"],correctAnswer: "Je te promets"},
+ {phrase: ".. les femmes qui pleurent leurs enfants mais restent dignes ..",options: ["Voici le monde", "Un rêve à faire", "Vivre pour le meilleur"],correctAnswer: "Vivre pour le meilleur"},
+ {phrase: ".. Empêche-moi de me détruire donne-moi, donne-moi ton corps ..",options: ["Chanter pour toi", "Derrière l'amour", "Cette fille-là"],correctAnswer: "Derrière l'amour"},
+ {phrase: ".. Et quand la troupe défile sous leurs carreaux ils ont l'âme guerrière..",options: ["Flagrant délit", "Il faut rouler", "Fils de personne"],correctAnswer: "Fils de personne"}
    
 ];
 
